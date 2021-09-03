c84a46c4d680922b926c0c38131c933f3ff3e10e
Walter Luna <wpenate@applaudostudios.com>
2021-09-03 14:14:27 -0600

    ✨ add endpoints to test guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
